717dd354a85489431a19a5cf4a1d39559c170871
Erkka Rahikainen <ergga96@gmail.com>
2021-12-31 00:45:57 +0200

    Progress

--- src/index.js
+++ src/index.js
@@ -1,24 +1,46 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
-
 import LoginForm from './Login'
+import {Button} from "react-bootstrap";
 import userinfo from './userinfo.json'
 
 const App = () => {
 
-  const [user, setUser] = useState(0)
+  const [user, setUser] = useState(undefined)
+  const users = userinfo.users
 
-  return(
+  console.log(users)
+
+  const logOut = () => {
+    setUser(undefined)
+    console.log("onnas")
+    return (
+      <div>
+      ${LoginForm(user,setUser())}</div>
+    )
+  }
+
+  const loggedInAs = () => {
+    return (
+      <div>
+        <p>Logged in as {user}</p>
+        <Button onClick={logOut}>Log out</Button>
+      </div>
+    )
+  }
+
+  const loginStatus = () => {
+    return (
+      user === undefined ? LoginForm(user, setUser) : loggedInAs()
+    )
+  }
+
+  return (
     <div>
       <h1>Welcome to SecureApp!</h1>
-      {loginStatus(user)}
+      {loginStatus(user, setUser)}
 
     </div>
-  )
-}
-const loginStatus = (props) => {
-  return (
-    props.user === 0 ? "Logged in as " : LoginForm()
   )
 }
 
