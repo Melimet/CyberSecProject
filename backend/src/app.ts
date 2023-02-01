1b0afd32c1baef00ea372729ac1664695404153d
Erkka Rahikainen <erkka.rahikainen@gmail.com>
2023-02-01 13:05:06 +0200

    Implement fix for flaw 5

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,11 @@ import express from 'express'
 import { userRouter } from './routes/user'
 import { loginRouter } from './routes/login'
 import cors from 'cors'
+import morgan from 'morgan'
 const app = express()
-// FIX: app.use(cors({origin: “http://localhost:3000”}))
+// FIX FLAW 4: app.use(cors({origin: “http://localhost:3000”}))
 app.use(cors())
+//FIX FLAW 5: app.use(morgan('tiny'))
 app.use(express.json())
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
