3a4075516d91288587690203ba011e30208cc188
Erkka Rahikainen <erkka.rahikainen@gmail.com>
2023-02-01 12:52:51 +0200

    Add fix for flaw 4

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import { userRouter } from './routes/user'
 import { loginRouter } from './routes/login'
 import cors from 'cors'
 const app = express()
+// FIX: app.use(cors({origin: “http://localhost:3000”}))
 app.use(cors())
 app.use(express.json())
 app.use('/api/users', userRouter)
